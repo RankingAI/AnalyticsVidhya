06f246dd5a82b0c3931d2841c5ac415d1cc62838
PayneJoe <yuanpingzhou@sensetime.com>
2017-09-13 14:37:30 +0800

    delete old entry file

--- BigMartSales3rd/src/entry/EntrySingleModel.py
+++
@@ -1,9 +0,0 @@
-from model.SingleModel import SingleModel
-
-if __name__ == '__main__':
-    """"""
-    InputDir = '/Users/yuanpingzhou/project/workspace/python/AnalyticsVidhya/BigMartSales3rd/data/FeatureEngineering'
-    OutputDir = '/Users/yuanpingzhou/project/workspace/python/AnalyticsVidhya/BigMartSales3rd/data/SingleModel'
-
-    strategies = ['lgb']
-    SingleModel.run(strategies, InputDir, OutputDir)
